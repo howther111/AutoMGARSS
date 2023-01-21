5f1034fecf5765ff134ce61a5a9fef1fa6c7f7df
Elauno Mitsugi <atsukiexfeel@yahoo.co.jp>
2023-01-21 11:46:20 +0900

    Add files via upload

---
+++ settings.py
@@ -0,0 +1,4 @@
+spread_sheet_url = "https://docs.google.com/spreadsheets/d/1W-H6YiJGatQWywcuw4tReQ-xp0AGtoe_aHgpizwJe-I/edit#gid=1393133532"
+x_cell_num = 9
+y_cell_num = 6
+porn_num = 16
---
+++ ssfinalmain.py
@@ -0,0 +1,5 @@
+import arreadss10
+import settings
+
+instance = arreadss10.imgToBoard(spread_sheet_url=settings.spread_sheet_url)
+instance.run()
